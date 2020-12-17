65fa6fe2a0dcfeb178e30e12cd131c42830001fe
saravorabbi <37805848+saravorabbi@users.noreply.github.com>
2020-12-18 00:35:21 +0100

    PHM_27
    - settata notifica giornaliera ogni 24 ore
    - sistemate notifiche giornaliere
    - aggiungi 5 report statici
    
    Da fare:
    - controlla notifiche del monitoraggio
    - controlli NewInfoActicity
    - aggiunti feature calendario: se il giorno ha un report allora la casella giorno ha il segnalino
    
    Macroargomenti da affrontare
    - filtro per vedere i report con valore >=3 (crea activity)
    - home activity con un design decente
    - mostra il report medio nella CalendarActivit

---
+++ PHM_27/app/src/main/java/com/example/personalhealthmonitor/database/SettingsViewModel.java
@@ -0,0 +1,126 @@
+package com.example.personalhealthmonitor.database;
+
+import android.app.Application;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+public class SettingsViewModel extends AndroidViewModel {
+
+    private String TAG = this.getClass().getSimpleName();
+
+    private InfoDao infoDao;
+    private InfoRoomDatabase infoDB;
+
+    public SettingsViewModel(@NonNull Application application) {
+        super(application);
+
+        infoDB = InfoRoomDatabase.getMyAppDatabase(application);
+        infoDao = infoDB.infoDao();
+    }
+
+    // Funzione chiamata quando finisco di usare il view model, distruggo l'oggetto
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        Log.i(TAG, "Settings ViewModel Distrutto");
+    }
+
+
+    // wrapper per l'inserimento della nota nel DB
+    public void insertSettings(Settings setting){
+        //devo eseguire questa operazione nel thread non UI -> uso async task
+        new SettingsViewModel.InsertAsyncTask(infoDao).execute(setting);
+    }
+
+    public void updateSettings(Settings settings){
+        new SettingsViewModel.UpdateAsyncTask(infoDao).execute(settings);
+    }
+
+
+    //ritorna la riga che ha il parametro passato (temperatura, peso etc.)
+    public LiveData<Settings> getSettingsFromParametro(String parametro){
+        return infoDao.getSettingsFromParametro(parametro);
+    }
+
+
+    //NOTIFICHE
+
+    public String getBeginDate(String parametro){
+        return infoDao.getBeginDate(parametro);
+    }
+
+    public String getEndDate(String parametro){
+        return infoDao.getEndDate(parametro);
+    }
+
+    public double getUpperBound(String parametro){
+        return infoDao.getUpperBound(parametro);
+    }
+
+    public double getLowerBound(String parametro){
+        return infoDao.getLowerBound(parametro);
+    }
+
+
+
+    // ================ //
+    // CLASSI ASINCRONE //
+    // ================ //
+
+    // Superclasse che generalizza le classi async
+    private class OperationAsyncTask extends AsyncTask<Settings, Void, Void> {
+
+        InfoDao asyncTaskDao;
+
+        //costruttore
+        OperationAsyncTask(InfoDao dao){
+            this.asyncTaskDao = dao;
+        }
+
+        @Override
+        protected Void doInBackground(Settings... settings) {
+            return null;
+        }
+    }
+
+
+    // Creo async task, per eseguire il task in background
+    // (deprecato, vedi se lo puoi sosttuire con qualcos'altro)!!!!!!!!!!!!!
+
+    //Async task per l'operazione Insert
+    private class InsertAsyncTask extends SettingsViewModel.OperationAsyncTask {
+
+        //costruttore
+        public InsertAsyncTask(InfoDao infoDao) {
+            super(infoDao);
+        }
+
+        @Override
+        protected Void doInBackground(Settings... settings){
+            asyncTaskDao.insertSettings(settings[0]);
+            return null;
+        }
+    }
+
+
+    //Async task per l'operazione Update
+    private class UpdateAsyncTask extends SettingsViewModel.OperationAsyncTask {
+
+        //costruttore
+        public UpdateAsyncTask(InfoDao infoDao) {
+            super(infoDao);
+        }
+
+        @Override
+        protected Void doInBackground(Settings... settings) {
+            asyncTaskDao.updateSettings(settings[0]);
+            return null;
+        }
+    }
+
+
+}
---
+++ PHM_27/app/src/main/java/com/example/personalhealthmonitor/notifications/AlertReceiver.java
@@ -0,0 +1,220 @@
+package com.example.personalhealthmonitor.notifications;
+
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.example.personalhealthmonitor.NewInfoActivity;
+import com.example.personalhealthmonitor.R;
+
+import java.util.Objects;
+
+import static com.example.personalhealthmonitor.utils.UtilsValue.*;
+import static com.example.personalhealthmonitor.notifications.App.CHANNEL_1_ID;
+
+public class AlertReceiver extends BroadcastReceiver {
+
+    private static final String TAG = "AlertReceiver: ";
+
+    private Bundle bundle;
+    private String notifyType;
+
+
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i(TAG, "ALERT MANAGER si è svegliato!");
+
+        bundle = intent.getExtras();
+
+        if(bundle != null) {
+
+            notifyType = bundle.getString("notification_type");
+
+
+
+            //daily notification
+            if( Objects.equals(notifyType, TIMED_NOTIFICATION) ){
+
+                Log.i(TAG, "Sono nella Timed Notification");
+
+                SharedPreferences prefs = context.getSharedPreferences(SHARED_PREF, Context.MODE_PRIVATE);
+                boolean aggiunto = prefs.getBoolean(NEW_REPORT, true);
+                Log.i(TAG, "NEW_REPORT = " + aggiunto);
+
+                if(aggiunto){
+                    //SI NOTIFICA
+                    Log.i(TAG, "SI NOTIFICA - devo aggiungere il report");
+
+                    createDailyNotification(context, intent);
+
+                }
+
+                else{
+                    //NO NOTIFICA
+                    Log.i(TAG, "NO NOTIFICA - ho già il report");
+
+                    //risetto NEW REPORT a true per la notifica del giorno dopo
+                    prefs.edit().putBoolean(NEW_REPORT, true).apply();
+                    boolean newRepo = prefs.getBoolean(NEW_REPORT, true);
+                    Log.i(TAG, "ADDED_REPO = " + newRepo );
+
+                }
+            }
+
+            //reminded notification
+            else if( Objects.equals(notifyType, REMIND_NOTIFICATION) ){
+
+                Log.i(TAG, "Sono nella Remind Me Later Notification");
+
+                //creo notifica giornaliera
+                createReminderNotification(context, intent);
+
+            }
+
+            //monitor notification
+            else if( Objects.equals(notifyType, SETTINGS_NOTIFICATION) ){
+
+                Log.i(TAG, "Sono nella Monitor Settings Notification");
+
+                createMonitorNotification(context, intent);
+
+            }
+
+
+
+        }else{
+            Log.i(TAG, "Beh, c'è un errore, non hai messo il bundle da qualche parte");
+        }
+
+
+        Log.i(TAG, "Fine del Broadcast Receiver");
+    }//end onReceive
+
+
+
+    private void createDailyNotification(Context context, Intent intent){
+
+        //Intent che apre l'app nella NEW ACTIVITY
+        Intent newInfoIntent = new Intent(context, NewInfoActivity.class);
+        PendingIntent pendingIntentNewInfo = PendingIntent.getActivity(context,2, newInfoIntent, 0);
+
+        //Intent che crea la Reminder Notification
+        Intent remindLaterIntent = new Intent(context, RemindLater.class);      //remindLaterIntent.putExtra("notification_type", REMIND_NOTIFICATION);
+        PendingIntent pendingIntentRemind = PendingIntent.getActivity(context, 3, remindLaterIntent, 0);
+
+        //creo la notifica che invio al cellulare
+        NotificationCompat.Builder dailyBuilder = new NotificationCompat.Builder(context, CHANNEL_1_ID)
+                .setSmallIcon(R.drawable.ic_baseline_local_hospital_24)
+                .setContentTitle("Aggiungi un nuovo Report")
+                .setContentText("Non hai ancora inserito le info sulla tua salute!")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_REMINDER)
+                .setColor(Color.BLUE)
+                .setContentIntent(pendingIntentNewInfo)    //quando faccio tap sulla notifica, apre la NEW ACTIVITY
+                .setAutoCancel(true)
+                .addAction(R.mipmap.ic_launcher, "Add ", pendingIntentNewInfo)    //quando faccio tap sulla notifica, si dismissa
+                .addAction(R.mipmap.ic_launcher, "Remind me later", pendingIntentRemind);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+
+        notificationManager.notify(ID_NOTIFICATION_DAILY, dailyBuilder.build());
+
+    }
+
+
+    private void createReminderNotification(Context context, Intent intent){
+
+        //Intent che apre l'app nella NEW ACTIVITY
+        Intent newInfoIntent = new Intent(context, NewInfoActivity.class);
+        PendingIntent pendingIntentNewInfo = PendingIntent.getActivity(context,5, newInfoIntent, 0);
+
+        //Intent che crea la Reminder Notification
+        Intent remindLaterIntent = new Intent(context, RemindLater.class);      //remindLaterIntent.putExtra("notification_type", REMIND_NOTIFICATION);
+        PendingIntent pendingIntentRemind = PendingIntent.getActivity(context, 6, remindLaterIntent, 0);
+
+        //creo la notifica che invio al cellulare
+        NotificationCompat.Builder dailyBuilder = new NotificationCompat.Builder(context, CHANNEL_1_ID)
+                .setSmallIcon(R.drawable.ic_baseline_local_hospital_24)
+                .setContentTitle("Aggiungi un nuovo Report - Reminder")
+                .setContentText("Non hai ancora inserito il report sulla tua salute!")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_REMINDER)
+                .setColor(Color.BLUE)
+                .setContentIntent(pendingIntentNewInfo)    //quando faccio tap sulla notifica, apre la NEW ACTIVITY
+                .setAutoCancel(true)
+                .addAction(R.mipmap.ic_launcher, "Add ", pendingIntentNewInfo)    //quando faccio tap sulla notifica, si dismissa
+                .addAction(R.mipmap.ic_launcher, "Remind me later", pendingIntentRemind);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+
+        notificationManager.notify(ID_NOTIFICATION_REMINDER, dailyBuilder.build());
+    }
+
+
+    private void createMonitorNotification(Context context, Intent intent){
+
+        boolean flag = false;
+        String text = "";
+
+        //check shared prefs -> per creare testo notifica
+
+        SharedPreferences prefs = context.getSharedPreferences(SHARED_PREF, Context.MODE_PRIVATE);
+
+        if( prefs.getBoolean(EXCEEDED_TEMP, true) ){
+            text += "Temperatura ";
+            flag = true;
+        }
+        if( prefs.getBoolean(EXCEEDED_PRESS_SIS, true) ){
+            text += "Pressione Sistolica ";
+            flag = true;
+        }
+        if( prefs.getBoolean(EXCEEDED_PRESS_DIA, true) ){
+            text += "Pressione Diastolica ";
+            flag = true;
+        }
+        if( prefs.getBoolean(EXCEEDED_GLIC, true) ){
+            text += "Glicemia";
+            flag = true;
+        }
+        if( prefs.getBoolean(EXCEEDED_PESO, true) ){
+            text += " Peso" ;
+            flag = true;
+        }
+
+
+        //manda notifica
+        if(flag) {
+            Log.i(TAG, "Monitor Notification: invio la notifica");
+
+            NotificationCompat.Builder dailyBuilder = new NotificationCompat.Builder(context, CHANNEL_1_ID)
+                    .setSmallIcon(R.drawable.ic_baseline_local_hospital_24)
+                    .setContentTitle("Monitor Notification - Soglia predefinitia superata")
+                    .setContentText(text)
+                    .setPriority(NotificationCompat.PRIORITY_HIGH)
+                    .setCategory(NotificationCompat.CATEGORY_REMINDER)
+                    .setColor(Color.RED)
+                    .setAutoCancel(true);
+
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+
+            notificationManager.notify(ID_NOTIFICATION_MONITOR, dailyBuilder.build());
+
+        }else{
+
+            Log.i(TAG, "Monitor Notification: non invio la notifica");
+
+        }
+    }
+
+
+}
